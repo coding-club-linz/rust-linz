6caec37c4eb5539113461d3ffb1b206f4a3432b8
SteinChr <c.steinberger@gibs.at>
2020-07-12 20:51:07 +0200

    Source Rust --> Constant

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ function sendContactForm(type) {
             url: 'https://prod-168.westeurope.logic.azure.com/workflows/2dd6d3e5e6704ed1afa801d769e4708b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=r26oUrw9V_-PToMEcu1HhY7w7-LQxyvK5H0M9U8knLc',
             contentType: 'application/json; charset=utf-8',
             dataType: 'json',
-            data: JSON.stringify({ email: $('#contact-email').val(), text: type + '\n\n' + $('#contact-text').val(), source: $('rust') })
+            data: JSON.stringify({ email: $('#contact-email').val(), text: type + '\n\n' + $('#contact-text').val(), source: 'rust' })
         }).done(function() {
             $('#contact-success').show();
         }).fail(function(error) {
